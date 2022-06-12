c047596c237db232b2c948b8cccbdd99c33bccec
Decode-Chan <aston.black39@gmail.com>
2022-06-11 21:56:45 -0400

    window scroll bug fix

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 let limitBottom = document.documentElement.offsetHeight - window.innerHeight;
 
 window.addEventListener("scroll", function() {
-    if (document.documentElement.scrollTop > 500) {
+    if (document.documentElement.scrollTop > 499.999999999999999999999999999999999999999999999993) {
         navBar.style.fontSize = "8px";
         navBar.style.padding = "6px";
     } else {
